87b5f0de7e19d966435879c21226eddcd6c2643b
Abhishek Kumar <abhishek9661342993@gmail.com>
2025-07-06 20:31:14 +0530

    feat: Enhance Card component with Split_Text prop for animated title display; update FeaturesProject layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,12 +3,10 @@ import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 gsap.registerPlugin(useGSAP);
 
-const Card = ({ Title, Image_Url ,  CardHoverText}) => {
+const Card = ({ Title, Image_Url, CardHoverText  , Split_Text}) => {
   let Card_Ref = React.useRef(null);
 
-
   let Handle_Card_Mouse_Enter = () => {
-
     gsap.to(Card_Ref.current, {
       scale: 0.96,
 
@@ -17,8 +15,6 @@ const Card = ({ Title, Image_Url ,  CardHoverText}) => {
   };
 
   let Handle_Card_Mouse_Leave = () => {
-
-    
     gsap.to(Card_Ref.current, {
       scale: 1,
 
@@ -26,25 +22,28 @@ const Card = ({ Title, Image_Url ,  CardHoverText}) => {
     });
   };
 
-
-
-    //   <div className="Title_Container flex items-baseline justify-center absolute transform translateX-1/2  translateY-1/2  z-30 w-full bg-red-700">
-    //       <h1 className='text-[#CDEA68] font-semibold text-[12vh]'>
-    //           {"Visa".split('').map((letter, index) => (
-    //             <span key={index} className={`inline-block transition-transform duration-300 ease-in-out transform `}>
-    //               {letter}
-    //             </span>
-    //           ))}
-          
-    //       </h1>
-    //       </div>
-
   useGSAP(() => {}, []);
   return (
-    <div className="Card_Container w-[48%] h-[90vh] font-[NeueMontriyan] ">
+    <div className="Card_Container w-[48%] h-[90vh] font-[NeueMontriyan] relative">
+      <div className="Title_Container flex items-baseline justify-center top-1/2 
+      
+      absolute transform translateX-1/2  
+      translateY-1/2  z-30 w-full bg-red-700">
+        <h1 className="text-[#CDEA68] font-semibold text-[12vh]">
+          {String(Split_Text).split('').map((letter, index) => (
+            <span
+              key={index}
+              className={`inline-block transition-transform duration-300 ease-in-out transform `}
+            >
+              {letter}
+            </span>
+          ))}
+        </h1>
+      </div>
+
       <div className="Title w-full h-[10%] flex items-center gap-5">
         <div className="Circle rounded-full bg-black w-3 h-3 "></div>
-        <p className='text-xl'>{Title}</p>
+        <p className="text-xl">{Title}</p>
       </div>
       <div
         className="Card h-[90%] w-full rounded-2xl"
@@ -55,8 +54,9 @@ const Card = ({ Title, Image_Url ,  CardHoverText}) => {
         <img
           className="w-full h-full bg-cover rounded-2xl"
           src={Image_Url}
-          alt=""
-          srcset=""
+
+          alt={Title}
+      
         />
       </div>
     </div>
